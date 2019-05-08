27166174f4d58969427afdceac4cdb947f0bc36c
tailer954 <44743933+tailer954@users.noreply.github.com>
2019-05-08 22:01:54 +0800

    delete

--- Code/signal_and_system_work1.m
+++
@@ -1,8 +0,0 @@
-N=12;
-M=[4 5 7 10];
-T=0:1:23;
-
-subplot(2,2,1);stem(T,sin(2*pi*M(1)*T/N),'-o'); xlabel('n');ylabel('Xm[n]');title('Xm[n]...M=4'); %以下的每一行便是一個圖形
-subplot(2,2,2);stem(T,sin(2*pi*M(2)*T/N),'-o'); xlabel('n');ylabel('Xm[n]');title('Xm[n]...M=5');
-subplot(2,2,3);stem(T,sin(2*pi*M(3)*T/N),'-o'); xlabel('n');ylabel('Xm[n]');title('Xm[n]...M=7');
-subplot(2,2,4);stem(T,sin(2*pi*M(4)*T/N),'-o'); xlabel('n');ylabel('Xm[n]');title('Xm[n]...M=10');
